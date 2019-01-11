d9430c9a6a293719faf1e242832ac479a2cbd840
rpuiu <raz.puiu@gmail.com>
2019-01-11 19:55:58 +0200

    Class to print first k largest elements

---
+++ src/main/java/klargestelements/ElementRemover.java
@@ -0,0 +1,39 @@
+package klargestelements;
+
+public class ElementRemover {
+
+    public static void main(String[] args) {
+        int[] array = new int[]{44,222,33,123213,412};
+        int noOfMax= 3;
+
+        for(int i = 0; i<noOfMax; i++){
+            int max = getMax(array);
+            System.out.println(max);
+            array = removeElementFromArray(max, array);
+
+        }
+
+    }
+    static int getMax(int[]array){
+        int max = array[0];
+        for (int elem:array){
+            if (elem>max){
+                max = elem;
+            }
+        }
+        return max;
+    }
+
+    static int[] removeElementFromArray(int element, int[]oldArray){
+        int[] newArray = new int[oldArray.length-1];
+
+        int index = 0;
+        for(int i = 0; i< oldArray.length; i++){
+            if(element!=oldArray[i]){
+                newArray[index]=oldArray[i];
+                index++;
+            }
+        }
+        return newArray;
+    }
+}
